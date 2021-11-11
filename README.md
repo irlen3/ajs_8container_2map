f839b52615c2df3e5191dfc3c327fc95f8631bc8
Irina Koval <irlen3@gmail.com>
2021-11-11 20:31:35 +0100

    inial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # ajs_6object_2destructuring
-[![Build status](https://ci.appveyor.com/api/projects/status/mhrgrdfvrkkdv9cf/branch/main?svg=true)](https://ci.appveyor.com/project/irlen3/ajs-6object-2destructuring/branch/main)
+[![Build status](https://ci.appveyor.com/api/projects/status/61yr41putcehd377/branch/master?svg=true)](https://ci.appveyor.com/project/irlen3/ajs-7regex/branch/master)
--- src/js/app.js
+++ src/js/app.js
@@ -1,40 +1,9 @@
-import orderByProperties from './sortProperties';
-import getMassive from './destructor';
+import validateUsername from './validationName';
 
 // task 1
-const hero = {
-  name: 'мечник',
-  health: 10,
-  level: 2,
-  attack: 80,
-  defence: 40,
-};
-const res = orderByProperties(hero, ['name', 'level']);
-console.log(res);
-
-// task 2
-const character = {
-  name: 'Лучник',
-  type: 'Bowman',
-  health: 50,
-  level: 3,
-  attack: 40,
-  defence: 10,
-  special: [
-    {
-      id: 8,
-      name: 'Двойной выстрел',
-      icon: 'http://...',
-      description: 'Двойной выстрел наносит двойной урон',
-    },
-    {
-      id: 9,
-      name: 'Нокаутирующий удар',
-      icon: 'http://...',
-      // <- обратите внимание, описание "засекречено"
-    },
-  ],
-};
-
-const result = getMassive(character);
-console.log(result);
+console.log(validateUsername('Петя'));
+console.log(validateUsername('Peter'));
+console.log(validateUsername('Pet+er'));
+console.log(validateUsername('fna97545kg'));
+console.log(validateUsername('fna%97kg'));
+console.log(validateUsername('4fna97kg'));
